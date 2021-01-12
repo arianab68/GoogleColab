7e653d470e2cfb5bf26099eefef1c580111f25a4
arianab68 <arianab68@gmail.com>
2021-01-12 09:37:15 -0500

    codingpuzzle

---
+++ PythonFiles/CodingPuzzle.py
@@ -0,0 +1,11 @@
+def sum_int(num):
+  sum = 0 #Starting at 0
+  
+  if num != int(num): #Checking If num is not an integer
+    print('Wrong input...BYE!')
+    print('0')
+
+  else:
+    for i in range (num + 1): #For every input of i i in the range of the value of num+1
+      sum += i #calculate the sum for every input of i 
+  return f'The sum is {total}'
